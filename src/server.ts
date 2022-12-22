2715a8aa7cccabb8ae50a7d423713f0be0b33635
lucianobs1 <lucianojob1@hotmail.com>
2022-12-22 14:19:14 -0300

    added initial configs with socket-io

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,25 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import path from "path";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
-app.listen(3000, () => console.log("server is running"));
+app.get("/", (request, response) => {
+  return response.json({
+    app: {
+      name: "Chat with socket.io",
+      tech: "WebSocket with socket.io",
+    },
+  });
+});
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+io.on("connection", (socket) => {
+  console.log("socket", socket.id);
+});
+
+server.listen(3000, () => console.log("server is running"));
