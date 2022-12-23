6b958a28361a8c3a3255f2514842112056808c10
lucianobs1 <lucianojob1@hotmail.com>
2022-12-23 12:52:30 -0300

    added feature to list all users in room

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,13 +6,54 @@ function onLoad() {
     avatar = urlParams.get("avatar"),
     email = urlParams.get("email");
 
-  console.log(name, email, avatar);
+  document.querySelector(".user_logged").innerHTML += `
+   <img
+    class="avatar_user_logged"
+    src=${avatar}
+    />
+    <strong id="user_logged">${name}</strong> 
+  `;
 
   socket.emit("start", {
     name,
     email,
     avatar,
   });
+
+  socket.on("new_users", (user) => {
+    const existInDiv = document.getElementById(`user_${data._id}`);
+
+    if (!existInDiv) {
+      addUser(data);
+    }
+  });
+
+  socket.emit("get_users", (users) => {
+    console.log("getUsers", users);
+
+    users.map((user) => {
+      if (user.email !== email) {
+        addUser(user);
+      }
+    });
+  });
 }
+
+const addUser = (user) => {
+  const usersList = document.getElementById("users_list");
+  usersList.innerHTML += `
+  <li
+    class="user_name_list"
+    id="user_${user._id}"
+    idUser="${user._id}"
+  >
+    <img
+      class="nav_avatar"
+      src=${user.avatar}
+    />
+    ${user.name}
+  </li> 
+  `;
+};
 
 onLoad();
--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,6 +1,7 @@
 import { container } from "tsyringe";
 import { io } from "../http";
 import { CreateUserService } from "../services/CreateUserService";
+import { GetAllUsersService } from "../services/GetAllUsersService";
 
 // io envio global de informações
 // socket controla o envio para alguns ou algum cliente
@@ -17,6 +18,14 @@ io.on("connect", (socket) => {
       socket_id: socket.id,
     });
 
-    console.log(user);
+    // Envia mensagem ou informação para todos os usuarios exceto a si mesmo
+    socket.broadcast.emit("new_users", user);
+  });
+
+  socket.on("get_users", async (callback) => {
+    const getAllUsersServices = container.resolve(GetAllUsersService);
+    const users = await getAllUsersServices.execute();
+
+    callback(users);
   });
 });
